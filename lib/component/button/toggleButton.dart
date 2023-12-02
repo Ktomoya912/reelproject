3c312334d0b2f84607d7c066d6acbcfd14948542
mitsuki <mitsuara0517@gmail.com>
2023-12-02 23:03:12 +0900

    通知画面のリファクタリングを行った

---
+++ lib/component/button/toggleButton.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '/provider/changeGeneralCorporation.dart';
+
+//ToggleButtonにて使用するボタンが押されているか否かの判定bool
+class ChangeToggleButton with ChangeNotifier {
+  var _toggleList = <bool>[true, false];
+  int onButtonIndex = 0;
+
+  void changeToggleList(i) {
+    switch (i) {
+      case 0:
+        if (_toggleList[1]) {
+          _toggleList = [true, false];
+          onButtonIndex = 0;
+        }
+        break;
+      case 1:
+        if (_toggleList[0]) {
+          _toggleList = [false, true];
+          onButtonIndex = 1;
+        }
+        break;
+    }
+    notifyListeners();
+  }
+}
+
+//ToggleButtonを作成するクラス
+class ToggleButton extends StatelessWidget {
+  final MediaQueryData mediaQueryData;
+  final String leftTitle;
+  final String rightTitle;
+  final double height;
+
+  const ToggleButton({
+    super.key,
+    required this.mediaQueryData, //context
+    required this.leftTitle, //左の文字
+    required this.rightTitle, //右の文字
+    required this.height, //ボタンの高さ
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final toggleStore = Provider.of<ChangeToggleButton>(context); //プロバイダ
+    final colorStore = Provider.of<ChangeGeneralCorporation>(context); //プロバイダ
+    return SizedBox(
+        height: height, //高さ
+        child: ToggleButtons(
+            //ボタンの文字と枠
+            children: <Widget>[
+              SizedBox(
+                  width: mediaQueryData.size.width / 2,
+                  child: Center(
+                      child: Text(leftTitle,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15)))),
+              SizedBox(
+                  width: mediaQueryData.size.width / 2 - 3,
+                  child: Center(
+                      child: Text(rightTitle,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15))))
+            ],
+            //色関連
+            color: colorStore.greyColor, //選択されていないときの色
+            selectedColor: colorStore.mainColor, //選択されているときの色
+            fillColor: colorStore.thinColor, //選択されているときの背景色
+            borderColor: colorStore.greyColor, //枠線の色
+            selectedBorderColor: colorStore.greyColor, //選択時の枠線の色
+            borderWidth: 1, //枠線の太さ
+            //ボタン選択関連
+            isSelected: toggleStore._toggleList, //ボタンが反応しているか否か
+            //ボタンが押された時の動作
+            onPressed: (index) {
+              toggleStore.changeToggleList(index);
+            }));
+  }
+}
--- lib/page/home/notice.dart
+++ lib/page/home/notice.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '/component/bottomAppBar/bNB.dart';
 import '/component/appBar/titleAppBar.dart';
+import '/component/Button/toggleButton.dart';
+import '/component/listView/NoticeListView.dart';
 
 class Notice extends StatefulWidget {
   @override
@@ -11,194 +14,50 @@ class _NoticeState extends State<Notice> {
   final int index = 0; //BottomAppBarのIcon番号
   final String title = "通知"; //AppBarに表示する文字
 
-  final List<Map<String, dynamic>> eventList = [
-    {"title": "【イベント開催間近】", "subtitle": "開催間近"},
-    {"title": "【イベント開催間近】", "subtitle": "開催間近"},
-    {"title": "【イベント開催間近】", "subtitle": "開催間近"},
-    {"title": "【イベント開催間近】", "subtitle": "開催間近"},
-    {"title": "【イベント開催間近】", "subtitle": "開催間近"}
+  List<List<Map<String, dynamic>>> noticeList = [
+    [
+      {"title": "【イベント開催間近】", "subtitle": "開催間近"},
+      {"title": "【イベント開催間近】", "subtitle": "開催間近"},
+    ],
+    [
+      {"title": "【イベント開催間近】", "subtitle": "開催間近"},
+      {"title": "【イベント開催間近】", "subtitle": "開催間近"},
+      {"title": "【イベント開催間近】", "subtitle": "開催間近"},
+    ]
   ];
-
-  final List<Map<String, dynamic>> jobList = [
-    {"title": "【求人期限間近】", "subtitle": "開催間近"},
-    {"title": "【求人期限間近】", "subtitle": "開催間近"},
-    {"title": "【求人期限間近】", "subtitle": "開催間近"},
-    {"title": "【求人期限間近】", "subtitle": "開催間近"},
-    {"title": "【求人期限間近】", "subtitle": "開催間近"},
-  ];
-
-  bool jedge = true; //イベント:true, 求人:false
-
-  //jedgeを変更し通知する
-  void jedgeChange(bool j) {
-    setState(() {
-      jedge = j;
-    });
-  }
-
-  //カラーをjedgeの値によって変更
-  Color colorJedge(bool j, Color trueColor, Color falseColor) {
-    if (j) {
-      return trueColor;
-    } else {
-      return falseColor;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData _mediaQueryData = MediaQuery.of(context);
-    return Scaffold(
-      //アップバー
-      appBar: TitleAppBar(title: title),
+    return ChangeNotifierProvider(
+        create: (context) => ChangeToggleButton(),
+        child: Builder(builder: (BuildContext context) {
+          final store = Provider.of<ChangeToggleButton>(context); //プロバイダ
+          return Scaffold(
+            //アップバー
+            appBar: TitleAppBar(title: title),
 
-      body: Column(
-        children: [
-          //イベント、求人切り替えボタン
-          //四角で囲む(上ボタンの幅選択)
-          Container(
-            height: 50, //高さ
-            decoration: const BoxDecoration(
-              //枠線
-              border: Border(
-                bottom: BorderSide(color: Colors.grey), //リストを区別する線
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            body: Column(
               children: [
-                //イベントボタン
-                SizedBox(
-                  height: 50, //ボタン高さ(上と同じ値)
-                  width: _mediaQueryData.size.width / 2, //幅調整
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: colorJedge(jedge, Colors.blue.shade300,
-                          Colors.white), //jedgeの値によってボタンの色変更
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0), //角の丸み
-                      ),
-                    ),
-                    onPressed: () => jedgeChange(true), //jedgeの値をtrueとする
-                    child: Text('イベント',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: colorJedge(jedge, Colors.white,
-                                Colors.blue))), //jedgeの値によって文字の色変更
-                  ),
+                //イベント、求人切り替えボタン
+                //四角で囲む(上ボタンの幅選択)
+
+                ToggleButton(
+                  mediaQueryData: _mediaQueryData,
+                  leftTitle: "イベント",
+                  rightTitle: "求人",
+                  height: 50,
                 ),
-                //求人ボタン
-                SizedBox(
-                  height: 50, //ボタン高さ(上と同じ値)
-                  width: _mediaQueryData.size.width / 2, //幅調整
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: colorJedge(jedge, Colors.white,
-                          Colors.blue.shade300), //jedgeの値によってボタンの色変更
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0), //角の丸み
-                      ),
-                    ),
-                    onPressed: () => jedgeChange(false), //jedgeの値をtrueとする
-                    child: Text('求人',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: colorJedge(jedge, Colors.blue,
-                                Colors.white))), //jedgeの値によって文字の色変更
-                  ),
-                ),
+
+                //リスト
+                NoticeListView(
+                    jedgeEJ: store.onButtonIndex, noticeList: noticeList),
               ],
             ),
-          ),
 
-          //リスト
-          ListViewChange(
-            jedge: jedge,
-            eventList: eventList,
-            jobList: jobList,
-          ),
-        ],
-      ),
-
-      //ボトムナビゲーションバー
-      bottomNavigationBar: BNB(index: index),
-    );
-  }
-}
-
-//通知一覧クラス
-class ListViewChange extends StatelessWidget {
-  const ListViewChange({
-    super.key,
-    required this.eventList,
-    required this.jobList,
-    required this.jedge,
-  });
-
-  //引数
-  final List<Map<String, dynamic>> eventList;
-  final List<Map<String, dynamic>> jobList;
-  final bool jedge;
-
-  //使用するリスト一覧
-  static List<Map<String, dynamic>> list = [{}];
-  static var icon = Icons.favorite;
-
-  @override
-  Widget build(BuildContext context) {
-    //jedgeによって使用するリスト決定
-    if (jedge) {
-      list = eventList;
-      icon = Icons.favorite;
-    } else {
-      list = jobList;
-      icon = Icons.work;
-    }
-
-    return Expanded(
-      //通知一覧
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return
-              //四角作成(これの大きさがリストの高さをになっている)
-              Container(
-                  height: 90, //リストの高さ
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.grey), //リストを区別する線
-                    ),
-                  ),
-                  //リストの内容
-                  child: Expanded(
-                      child: ListTile(
-                          //左のアイコン
-                          leading: Container(
-                              height: 70, //アイコン高さ
-                              width: 70, //アイコン幅
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle, //円形に
-                                  color: Colors.orange.shade100), //アイコン周囲円の色
-                              //円内のアイコン
-                              child: Icon(icon,
-                                  size: 45, color: Colors.orange)), //アイコンの色
-                          //右側の矢印アイコン
-                          trailing: Padding(
-                            padding: const EdgeInsets.all(
-                                15.0), //このままだと真ん中に来ないため空間を作る
-                            child: Icon(
-                              Icons.arrow_forward_ios,
-                              size: 30,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          title: Text(list[index]["title"]), //タイトル
-                          subtitle: Text(list[index]["subtitle"]), //サブタイトル
-                          visualDensity: VisualDensity(
-                              vertical: 1.5), //listTitleの大きさを広げている(1.5倍)
-                          onTap: () {}))); //ボタンを押した際の挙動
-        },
-        itemCount: list.length, //リスト数
-      ),
-    );
+            //ボトムナビゲーションバー
+            bottomNavigationBar: BNB(index: index),
+          );
+        }));
   }
 }
