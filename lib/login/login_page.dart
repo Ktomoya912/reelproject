71b948f029130f5306f1d0d38c17934b6042cb77
Shoei <250383h@ugs.kochi-tech.ac.jp>
2023-12-04 21:56:26 +0900

    ログイン前のAppBarをコンポーネント化し、新規会員登録画面を追加記述

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -3,8 +3,8 @@ import 'package:reelproject/login/forgot_password_page.dart';
 import 'package:reelproject/login/new_menber_general.dart';
 import 'package:reelproject/provider/changeGeneralCorporation.dart';
 import 'package:reelproject/page/home/home.dart';
-import 'package:provider/provider.dart'; //パッケージをインポート
-//パスを指定して、forgot_password_page.dartをインポート
+import 'package:provider/provider.dart';
+import '/component/appBar/loginAppBar.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -18,7 +18,6 @@ class LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     super.initState();
-    //シード値を設定することによって関連の色を生成するようにする
   }
 
   @override
@@ -138,7 +137,7 @@ class LoginPageState extends State<LoginPage> {
                           );
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: store.subColor, //onPrimaryは非推奨らしい
+                          backgroundColor: store.subColor,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
@@ -200,69 +199,5 @@ class LoginPageState extends State<LoginPage> {
             ),
           );
         }));
-  }
-}
-
-class LoginAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const LoginAppBar({
-    super.key,
-    required this.store,
-  });
-
-  final ChangeGeneralCorporation store;
-
-  @override
-  Size get preferredSize => const Size.fromHeight(80);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      //アップバータイトル
-      title: const Text(
-        "REEL", //文字
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 44,
-            color: Colors.white), //書体
-      ),
-      backgroundColor: store.mainColor, //背景
-      iconTheme: const IconThemeData(color: Colors.grey), //戻るボタン
-      centerTitle: true, //中央揃え
-      toolbarHeight: 80, //アップバーの高さ
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20, top: 50),
-          child: InkWell(
-            onTap: () {
-              // パスワードを忘れた場合の画面に遷移
-
-              store.changeGC(!store.jedgeGC);
-            },
-            splashColor: Colors.transparent, // splashColorを透明にする。
-            child: store.jedgeGC
-                ? const Text(
-                    '法人の方はこちら',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.white,
-                      decorationThickness: 2,
-                      color: Colors.white,
-                    ),
-                  )
-                : const Text(
-                    '個人の方はこちら',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.white,
-                      decorationThickness: 2,
-                      color: Colors.white,
-                    ),
-                  ),
-          ),
-        ),
-      ],
-    );
   }
 }
