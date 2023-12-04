816071cb3caa57b0a7e93c20307c71e8801cbfbb
mitsuki <mitsuara0517@gmail.com>
2023-12-04 17:22:27 +0900

    loginAppBarの削除

--- lib/page/event/event.dart
+++ lib/page/event/event.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '/component/bottomAppBar/bNB.dart';
-import '/component/appBar/loginAppBar.dart';
 
 class Event extends StatefulWidget {
   @override
@@ -12,8 +11,6 @@ class _EventState extends State<Event> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //ボトムナビゲーションバー
-      appBar: LoginAppBar(),
       bottomNavigationBar: BNB(index: index),
     );
   }
