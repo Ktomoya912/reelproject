a60f38aacac4e6bf4a302c63977bf24843715a2d
mitsuki <mitsuara0517@gmail.com>
2023-12-06 16:16:19 +0900

    #51 #59 解決

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -27,182 +27,176 @@ class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final store = Provider.of<ChangeGeneralCorporation>(context);
-    return ChangeNotifierProvider(
-      create: (context) => ChangeGeneralCorporation(),
-      child: Builder(builder: (BuildContext context) {
-        return Scaffold(
-          appBar: LoginAppBar(store: store),
-          body: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      appBar: LoginAppBar(store: store),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'ログイン',
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  if (store.jedgeGC) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const NewMemberGeneral()),
+                    );
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const NewMemberCompany()),
+                    );
+                  }
+                },
+                splashColor: Colors.transparent,
+                child: const Text(
+                  '新規会員登録はこちら',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
+                  const Text(
+                    'メールアドレス',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 300,
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        hintText: '例：info@example.com',
+                      ),
+                    ),
+                  ),
                   const SizedBox(
                     height: 20,
                   ),
                   const Text(
-                    'ログイン',
+                    'パスワード',
                     style: TextStyle(
-                      fontSize: 34,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      if (store.jedgeGC) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const NewMemberGeneral()),
-                        );
-                      } else {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const NewMemberCompany()),
-                        );
-                      }
-                    },
-                    splashColor: Colors.transparent,
-                    child: const Text(
-                      '新規会員登録はこちら',
-                      style: TextStyle(color: Colors.blue),
+                      fontSize: 15,
                     ),
                   ),
                   const SizedBox(
-                    height: 80,
+                    width: 300,
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        hintText: '例：password',
+                      ),
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text(
-                        'メールアドレス',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 300,
-                        child: TextField(
-                          textAlign: TextAlign.start,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 10),
-                            hintText: '例：info@example.com',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Checkbox(
+                        value: _autoLogin,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _autoLogin = value ?? false;
+                          });
+                        },
                       ),
                       const Text(
-                        'パスワード',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 300,
-                        child: TextField(
-                          textAlign: TextAlign.start,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 10),
-                            hintText: '例：password',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Checkbox(
-                            value: _autoLogin,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                _autoLogin = value ?? false;
-                              });
-                            },
-                          ),
-                          const Text(
-                            '次回から自動でログインする',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                        onPressed: () => context.navigateTo(const RootRoute()),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: store.subColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          minimumSize: const Size(300, 50),
-                        ),
-                        child: const Text('ログイン'),
-                      ),
-                      const SizedBox(
-                        height: 20,
+                        '次回から自動でログインする',
+                        style: TextStyle(fontSize: 15),
                       ),
                     ],
                   ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PassChange()),
-                      );
-                    },
-                    splashColor: Colors.transparent,
-                    child: const Text(
-                      'パスワードを忘れた方はこちら',
-                      style: TextStyle(color: Colors.blue),
+                  ElevatedButton(
+                    onPressed: () => context.navigateTo(const RootRoute()),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: store.subColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      minimumSize: const Size(300, 50),
                     ),
+                    child: const Text('ログイン'),
                   ),
                   const SizedBox(
-                    height: 100,
+                    height: 20,
                   ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            // builder: (context) => const ForgotPasswordPage()),
-                            builder: (context) => const AskPage()),
-                      );
-                    },
-                    splashColor: Colors.transparent,
-                    child: const Text(
-                      'お問い合わせはこちら',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  )
                 ],
               ),
-            ),
-          ),
-          bottomNavigationBar: BottomAppBar(
-            color: store.mainColor,
-            height: 40,
-            child: const Text(
-              '© 2023 REEL',
-              style: TextStyle(
-                color: Colors.white,
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const PassChange()),
+                  );
+                },
+                splashColor: Colors.transparent,
+                child: const Text(
+                  'パスワードを忘れた方はこちら',
+                  style: TextStyle(color: Colors.blue),
+                ),
               ),
-              textAlign: TextAlign.center,
-            ),
+              const SizedBox(
+                height: 100,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        // builder: (context) => const ForgotPasswordPage()),
+                        builder: (context) => const AskPage()),
+                  );
+                },
+                splashColor: Colors.transparent,
+                child: const Text(
+                  'お問い合わせはこちら',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
           ),
-        );
-      }),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: store.mainColor,
+        height: 40,
+        child: const Text(
+          '© 2023 REEL',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
