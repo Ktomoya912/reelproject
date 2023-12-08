3a8ba105d94e513592f7b4f585ce56c22a9e3976
mitsuki <mitsuara0517@gmail.com>
2023-12-08 15:26:37 +0900

    Componentのレイヤーへの投稿

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import '../../component/appbar/main_appbar.dart';
 import 'package:auto_route/auto_route.dart';
 import '/page/home/notice.dart';
+import 'package:provider/provider.dart';
+import '/provider/change_general_corporation.dart';
 
 @RoutePage()
 class HomeRouterPage extends AutoRouter {
@@ -24,5 +25,58 @@ class _HomeState extends State<Home> {
       //アップバー
       appBar: MainAppBar(nextPage: Notice()),
     );
+  }
+}
+
+//使い方
+//ファイルの上部でimport '.mainAppBar.dart';と置く
+//その後、Scaffold内で"appBar: MainAppBar(nextPage: notice())""のように宣言
+//この時のnextPageには移動先クラスを置く
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget nextPage; //通知ボタンから移動するページ
+
+  const MainAppBar({
+    super.key,
+    required this.nextPage,
+  });
+
+  @override
+  Size get preferredSize {
+    return const Size(double.infinity, 80.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<ChangeGeneralCorporation>(context); //プロバイダ
+    return AppBar(
+        //アップバータイトル
+        title: Text(
+          "REEL", //文字
+          style: TextStyle(
+              color: store.mainColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 40), //書体
+        ),
+        backgroundColor: store.subColor,
+        elevation: 0.0, //影なし
+        toolbarHeight: 100, //アップバーの高さ
+        automaticallyImplyLeading: false, //戻るボタンの非表示
+        centerTitle: true,
+        //アップバーアイコン
+        actions: <Widget>[
+          //通知ボタン
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            color: store.mainColor,
+            //通知ページへ移動(push)
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          nextPage));
+            },
+          )
+        ]);
   }
 }
--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -7,7 +7,6 @@ import 'package:reelproject/page/login/ask_page.dart';
 import 'package:reelproject/provider/change_general_corporation.dart';
 import 'package:reelproject/page/login/pass_change.dart';
 import 'package:provider/provider.dart';
-import '../../component/appbar/login_appbar.dart';
 
 @RoutePage()
 class LoginPage extends StatefulWidget {
@@ -197,6 +196,68 @@ class LoginPageState extends State<LoginPage> {
           textAlign: TextAlign.center,
         ),
       ),
+    );
+  }
+}
+
+class LoginAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const LoginAppBar({
+    super.key,
+    required this.store,
+  });
+
+  final ChangeGeneralCorporation store;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      //アップバータイトル
+      title: const Text(
+        "REEL", //文字
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 44,
+            color: Colors.white), //書体
+      ),
+      backgroundColor: store.mainColor, //背景
+      iconTheme: const IconThemeData(color: Colors.grey), //戻るボタン
+      centerTitle: true, //中央揃え
+      toolbarHeight: 80, //アップバーの高さ
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 50),
+          child: InkWell(
+            onTap: () {
+              store.changeGC(!store.jedgeGC);
+            },
+            splashColor: Colors.transparent, // splashColorを透明にする。
+            child: store.jedgeGC
+                ? const Text(
+                    '法人の方はこちら',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.white,
+                      decorationThickness: 2,
+                      color: Colors.white,
+                    ),
+                  )
+                : const Text(
+                    '個人の方はこちら',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.white,
+                      decorationThickness: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+          ),
+        ),
+      ],
     );
   }
 }
