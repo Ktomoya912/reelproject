600ae09181978434af2e72902d41451e85d170eb
mitsuki <mitsuara0517@gmail.com>
2023-12-24 01:06:33 +0900

    求人の広告リストを作成

--- lib/page/job/job.dart
+++ lib/page/job/job.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:reelproject/component/appbar/event_job_appbar.dart';
+import 'package:reelproject/component/listView/job_advertisment_list.dart';
 
 //求人ページ
 @RoutePage()
@@ -45,16 +46,54 @@ class _JobState extends State<Job> {
     },
   ];
 
+  //求人広告のリスト
+  //titleに文字数制限を設ける
+  static List<Map<String, dynamic>> advertisementList = [
+    {
+      "title": "居酒屋新谷スタッフ募集", //タイトル
+      "pay": "900", //時給
+      "time": null, //時間
+      "place": "香美市土佐山田町000", //場所
+    },
+    {
+      "title": "川上神社夏祭り2",
+      "pay": "2021/8/1",
+      "time": "10:00~20:00",
+      "place": "香美市川上町",
+    },
+    {
+      "title": "川上神社夏祭り3",
+      "pay": "2021/8/1",
+      "time": "10:00~20:00",
+      "place": "香美市川上町",
+    },
+    {
+      "title": "川上神社夏祭り4",
+      "pay": "2021/8/1",
+      "time": "10:00~20:00",
+      "place": "香美市川上町",
+    },
+    {
+      "title": "川上神社夏祭り5",
+      "pay": "2021/8/1",
+      "time": "10:00~20:00",
+      "place": "香美市川上町",
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context); //画面サイズ取得
     return Scaffold(
-      appBar: EventJobSearchBar(
-        tagList: tagList,
-        favoriteHistoryList: favoriteHistoryList,
-        title: "おすすめ求人",
-        mediaQueryData: mediaQueryData,
-      ),
-    );
+        appBar: EventJobSearchBar(
+          tagList: tagList,
+          favoriteHistoryList: favoriteHistoryList,
+          title: "おすすめ求人",
+          mediaQueryData: mediaQueryData,
+        ),
+        body: JobAdvertisementList(
+          advertisementList: advertisementList,
+          mediaQueryData: mediaQueryData,
+        ));
   }
 }
