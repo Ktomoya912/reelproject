968efb7c0e66191492ffb0be223b7337445ea06c
mitsuki <mitsuara0517@gmail.com>
2024-01-04 18:38:17 +0900

    通知画面のエラー解消

--- lib/page/home/notice.dart
+++ lib/page/home/notice.dart
@@ -82,7 +82,7 @@ class _NoticeState extends State<Notice> {
                   //リスト
                   SizedBox(
                     width: width + (widthBlank / 8) + blank - 10,
-                    height: mediaQueryData.size.height - 210,
+                    height: mediaQueryData.size.height - 230,
                     child: NoticeListView(
                       jedgeEJ: store.onButtonIndex,
                       noticeList: noticeList,
@@ -117,64 +117,69 @@ class NoticeListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = Provider.of<ChangeGeneralCorporation>(context); //プロバイダ
     //ListViewをColumnの中にいれるにはExpandedを行う必要がある
-    return Expanded(
-      //通知一覧
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return
-              //四角作成(これの大きさがリストの高さをになっている)
-              Container(
-                  height: 90, //リストの高さ
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.grey), //リストを区別する線
-                    ),
-                  ),
-                  //リストの内容
-                  //リストの一つ一つを作成するListTitle
-                  child: ListTile(
-                      //左のアイコン
-                      //Containerで円を作っている
-                      leading: Container(
-                          height: 70, //アイコン高さ
-                          width: 70, //アイコン幅
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle, //円形に
-                              color: store.subColor), //アイコン周囲円の色
-                          //円内のアイコン
-                          child: Icon(icon[jedgeEJ],
-                              size: 45, color: store.mainColor)), //アイコンの色
-                      //右側の矢印アイコン
-                      trailing: Padding(
-                        padding:
-                            const EdgeInsets.all(15.0), //このままだと真ん中に来ないため空間を作る
-                        child: Icon(
-                          Icons.arrow_forward_ios,
-                          size: 30,
-                          color: store.greyColor,
+    return Column(
+      children: [
+        Expanded(
+          //通知一覧
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return
+                  //四角作成(これの大きさがリストの高さをになっている)
+                  Container(
+                      height: 90, //リストの高さ
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.grey), //リストを区別する線
                         ),
                       ),
-                      title: Text(noticeList[jedgeEJ][index]["title"]), //タイトル
-                      subtitle:
-                          Text(noticeList[jedgeEJ][index]["subtitle"]), //サブタイトル
-                      visualDensity: const VisualDensity(
-                          vertical: 1.5), //listTitleの大きさを広げている(1.5倍)
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) =>
+                      //リストの内容
+                      //リストの一つ一つを作成するListTitle
+                      child: ListTile(
+                          //左のアイコン
+                          //Containerで円を作っている
+                          leading: Container(
+                              height: 70, //アイコン高さ
+                              width: 70, //アイコン幅
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle, //円形に
+                                  color: store.subColor), //アイコン周囲円の色
+                              //円内のアイコン
+                              child: Icon(icon[jedgeEJ],
+                                  size: 45, color: store.mainColor)), //アイコンの色
+                          //右側の矢印アイコン
+                          trailing: Padding(
+                            padding: const EdgeInsets.all(
+                                15.0), //このままだと真ん中に来ないため空間を作る
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 30,
+                              color: store.greyColor,
+                            ),
+                          ),
+                          title:
+                              Text(noticeList[jedgeEJ][index]["title"]), //タイトル
+                          subtitle: Text(
+                              noticeList[jedgeEJ][index]["subtitle"]), //サブタイトル
+                          visualDensity: const VisualDensity(
+                              vertical: 1.5), //listTitleの大きさを広げている(1.5倍)
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                    pageBuilder: (context, animation,
+                                            secondaryAnimation) =>
                                         NoticeDetail(
                                           noticeList: noticeList,
                                           jedgeEJ: jedgeEJ,
                                           index: index,
                                           content: content,
                                         )));
-                      })); //ボタンを押した際の挙動
-        },
-        itemCount: noticeList[jedgeEJ].length, //リスト数
-      ),
+                          })); //ボタンを押した際の挙動
+            },
+            itemCount: noticeList[jedgeEJ].length, //リスト数
+          ),
+        ),
+      ],
     );
   }
 }
